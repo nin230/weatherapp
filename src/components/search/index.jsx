c990a9c71bc197188faabb753107d95552766e1b
sarvesh jain <sarvesh.jain2017@gmail.com>
2024-08-11 15:52:35 +0530

    first commit

---
+++ src/components/search/index.jsx
@@ -0,0 +1,15 @@
+
+
+export default function Search({search,setSearch,handleSearch}){
+    return  <div className="search-engine">
+        <input
+        type ="text"
+        className="city-search"
+        placeholder="enter city name"
+        name= "search"
+        value= {search}
+        onChange= {(event)=> setSearch(event.target.value)}
+        />
+        <button className="search btn" onClick={handleSearch}>Search Weather</button>
+    </div>
+}
---
+++ src/components/weather/index.jsx
@@ -0,0 +1,36 @@
+
+import { useState } from "react";
+import Search from "../search"
+
+export default function Weather(){
+
+    const[search,setSearch] = useState('');
+    const[loading,setLoading] = useState(false);
+    const[weatherData,setweatherData]= useState(null);
+
+    async function fetchWeatherData(param){
+        try{
+            const response = await fetch(
+                `https://api.openweathermap.org/data/2.5/weather?q=${param}&appid=f6e8392d478d66e3d7ffcbc63289c58d`
+              );
+
+            const data = await response.json();
+            console.log(data,'data');
+        }
+        catch(e){
+            console.log(e); 
+        }
+    }
+     
+    async function handleSearch(){
+       fetchWeatherData(search)
+    }
+    return <div>
+        <Search 
+        search={search}
+        setSearch={setSearch}
+        handleSearch={handleSearch}
+        />
+        Weather
+    </div>
+}
