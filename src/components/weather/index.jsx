f3be6f11d357410c5c4fbdfa710cf6a0e26f1246
sarvesh jain <sarvesh.jain2017@gmail.com>
2024-08-11 17:16:49 +0530

    changes

--- src/components/weather/index.jsx
+++ src/components/weather/index.jsx
@@ -1,43 +1,86 @@
 
 import { useState } from "react";
 import Search from "../search"
+import { useEffect } from "react";
 
-export default function Weather(){
+export default function Weather() {
 
-    const[search,setSearch] = useState('');
-    const[loading,setLoading] = useState(false);
-    const[weatherData,setweatherData]= useState(null);
+    const [search, setSearch] = useState('');
+    const [loading, setLoading] = useState(false);
+    const [weatherData, setweatherData] = useState(null);
 
-    async function fetchWeatherData(param){
+    async function fetchWeatherData(param) {
         setLoading(true);
-        try{
+        try {
             const response = await fetch(
                 `https://api.openweathermap.org/data/2.5/weather?q=${param}&appid=f6e8392d478d66e3d7ffcbc63289c58d`
-              );
+            );
 
             const data = await response.json();
-            console.log(data,'data');
-            if(data){ 
+            console.log(data, 'data');
+            if (data) {
                 setweatherData(data);
                 setLoading(false);
             }
         }
-        catch(e){
+        catch (e) {
             setLoading(false);
-            console.log(e); 
+            console.log(e);
         }
     }
-     
-    async function handleSearch(){
-       fetchWeatherData(search)
+
+    async function handleSearch() {
+        fetchWeatherData(search)
     }
-    console.log(loading);
+
+    function getCurrentDate() {
+        return new Date().toLocaleDateString("en-us", {
+            weekday: "long",
+            month: "long",
+            day: "numeric",
+            year: "numeric",
+        });
+    }
+    useEffect(() => {
+        fetchWeatherData("Bengaluru");
+    }, []);
+
+    console.log(weatherData);
     return <div>
-        <Search 
-        search={search}
-        setSearch={setSearch}
-        handleSearch={handleSearch}
-        />
-        Weather
+        <Search
+            search={search}
+            setSearch={setSearch}
+            handleSearch={handleSearch}
+        />{
+            loading ? <div>Loading...</div> :
+                <div>
+                    <div className="city-name">
+                        <h2>{weatherData?.name}, <span>{weatherData?.sys?.country}</span></h2>
+                    </div>
+                    <div className="date">
+                        <span>{getCurrentDate()}</span>
+                    </div>
+                    <div className="temp">{weatherData?.main?.temp}</div>
+                    <p className="description">
+                        {weatherData && weatherData.weather && weatherData.weather[0]
+                            ? weatherData.weather[0].description
+                            : ""}
+                    </p>
+                    <div className="weather-info">
+            <div className="column">
+              <div>
+                <p className="wind">{weatherData?.wind?.speed}</p>
+                <p>Wind Speed</p>
+              </div>
+            </div>
+            <div className="column">
+              <div>
+                <p className="humidity">{weatherData?.main?.humidity}%</p>
+                <p>Humidity</p>
+              </div>
+            </div>
+          </div>
+                </div>
+        }
     </div>
 }
